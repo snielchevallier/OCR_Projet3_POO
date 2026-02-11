61035bfd197097deda2b35c345df26e0c92522de
snielchevallier <snielchevallier@gmail.com>
2026-02-11 17:55:01 +0100

    gestion des commandes CLI

--- main.php
+++ main.php
@@ -1,6 +1,15 @@
 <?php
 
 while (true) {
-    $line = readline("Entrez votre commande : ");
-    echo "Vous avez saisi : $line\n";
+    $line = readline("Entrez votre commande (help, list, detail, create, delete, exit) :");
+    switch ($line){
+        case "list":
+            echo "affichage de la liste" . PHP_EOL;
+            break;
+        case "exit":
+            echo "au revoir" . PHP_EOL;
+            exit(0);
+        default:
+            echo "commande inconnue - consultez l'aide: aide + entrée" . PHP_EOL;
+    }
 }
